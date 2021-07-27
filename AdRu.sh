de4da0cb820cf71bf5efff925de65e1c95dcd247
p1xel0 <p1xel@NanoS.localdomain>
2021-07-27 17:23:49 -0400

    update to use nyaa RSS since subsplease.org RSS isn't working

--- AdRu.sh
+++ AdRu.sh
@@ -8,22 +8,21 @@ command -v rclone &> /dev/null || { echo 'error: rclone is not installed' 1>&2; 
 command -v anititle &> /dev/null || { echo 'error: anititle is not installed' 1>&2; exit 1; }
 mkdir -p ~/HorribleSubs
 cd ~/HorribleSubs || { echo 'error: failed to cd ~/HorribleSubs/' 1>&2; exit 1; }
-aria2c -q --remove-control-file=true --allow-overwrite=true 'https://subsplease.org/rss/?r=1080' -o .rss.txt || { echo 'error: failed to retrieve RSS' 1>&2; exit 1; }
-grep -q "<title>SubsPlease RSS</title>" .rss.txt || { rm .rss.txt; echo 'error: downloaded data was not SP RSS' 1>&2; exit 1; }
-sed -i 's/<link>/\n&/g;s/\&amp\;/\&/g' .rss.txt
-sed -n -i 's:.*<link>\(.*\)</link>.*:\1:p' .rss.txt
-sed -i -e "/https\:\/\/subsplease\.org/d;\$a\\" .rss.txt
+aria2c -q --remove-control-file=true --allow-overwrite=true 'https://nyaa.si/?page=rss&u=subsplease' -o .rss.txt || { echo 'error: failed to retrieve RSS' 1>&2; exit 1; }
+sed -n '/<title>.*\(1080p\).*<\/title>/{n;p}' -i .rss.txt
+sed -n 's:.*<link>\(.*\)</link>.*:\1:p' -i .rss.txt
+grep -q ^https.*torrent$ .rss.txt || { rm .rss.txt; echo 'error: downloaded data was not SP RSS' 1>&2; exit 1; }
 if [ -f ./.rss.old ]; then
 	if [ -n "$(diff -q .rss.old .rss.txt)" ]; then
 		diff ./.rss.old ./.rss.txt | grep \> | sed 's/^..//' > rss.txt
-		aria2c -V --seed-time=0 -i rss.txt
+		aria2c -V --seed-time=0 --follow-torrent=mem -i rss.txt
 		mv .rss.txt .rss.old && rm rss.txt
 	else
 		rm .rss.txt
 	fi
 else
 	mv .rss.txt rss.txt
-	aria2c -V --seed-time=0 -i rss.txt
+	aria2c -V --seed-time=0 --follow-torrent=mem -i rss.txt
 	mv rss.txt .rss.old
 fi
 if [ -n "$(ls -- \[SubsPlease\]\ */ 2> /dev/null)" ]; then
